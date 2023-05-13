8043587f16b5e16ee6489cf5a3b161d537e95180
Jeronimo <jeronimotornini@gmail.com>
2023-05-12 22:54:29 -0300

    Datos de Productos

---
+++ src/Data/Products.js
@@ -0,0 +1,38 @@
+export const Products = [
+  {
+    id: "1",
+    name: "Bag",
+    image: require("../Products/product1.jpg"),
+    price: 1000,
+  },
+  {
+    id: "2",
+    name: "Black Jacket",
+    image: require("../Products/product2.jpg"),
+    price: 1200,
+  },
+  {
+    id: "3",
+    name: "Purple Jacket",
+    image: require("../Products/product3.jpg"),
+    price: 1400,
+  },
+  {
+    id: "4",
+    name: "Shirt",
+    image: require("../Products/product4.jpg"),
+    price: 1600,
+  },
+  {
+    id: "5",
+    name: "Leggin",
+    image: require("../Products/product5.jpg"),
+    price: 1800,
+  },
+  {
+    id: "6",
+    name: "Tablet",
+    image: require("../Products/product6.jpg"),
+    price: 2000,
+  },
+];
