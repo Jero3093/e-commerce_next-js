82955565aa31a59df67a10b01df5b7478770fd18
Jeronimo <jeronimotornini@gmail.com>
2023-05-14 00:14:06 -0300

    Productos con Descripcion

--- src/Data/Products.js
+++ src/Data/Products.js
@@ -2,6 +2,8 @@ export const Products = [
   {
     id: "1",
     name: "Bag",
+    description:
+      "This is a stylish and durable bag that's perfect for everyday use.",
     image: require("../Products/product1.jpg"),
     price: 1000,
     category: "Bags",
@@ -9,36 +11,46 @@ export const Products = [
   {
     id: "2",
     name: "Black Jacket",
+    description:
+      "Stay warm and stylish with this black jacket made with high-quality materials.",
     image: require("../Products/product2.jpg"),
     price: 1200,
-    category: "Bags",
+    category: "Jackets",
   },
   {
     id: "3",
     name: "Purple Jacket",
+    description:
+      "Add a pop of color to your wardrobe with this stylish and comfortable purple jacket.",
     image: require("../Products/product3.jpg"),
     price: 1400,
-    category: "Bags",
+    category: "Jackets",
   },
   {
     id: "4",
     name: "Shirt",
+    description:
+      "Stay cool and comfortable with this lightweight and breathable shirt.",
     image: require("../Products/product4.jpg"),
     price: 1600,
-    category: "Bags",
+    category: "Shirts",
   },
   {
     id: "5",
     name: "Leggin",
+    description:
+      "Get the perfect fit and comfort with these high-quality leggings made with stretchy fabric.",
     image: require("../Products/product5.jpg"),
     price: 1800,
-    category: "Bags",
+    category: "Pants",
   },
   {
     id: "6",
     name: "Tablet",
+    description:
+      "Stay connected and productive on-the-go with this powerful and versatile tablet.",
     image: require("../Products/product6.jpg"),
     price: 2000,
-    category: "Bags",
+    category: "Electronics",
   },
 ];
