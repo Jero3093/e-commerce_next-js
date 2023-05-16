413ad8c9428f1ef32010e14aab695ec3e1973e65
Jeronimo <jeronimotornini@gmail.com>
2023-05-15 22:26:03 -0300

    Componente Footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return <div className="flex flex-col gap-y-2 items-center mt-7 mb-2">
+    <p className="text-lg text-zinc-600">E-Commerce</p>
+    <p className="text-zinc-600">Copyright© 2023</p>
+  </div>;
+}
+
+export default Footer;
