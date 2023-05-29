dbf5cef8106a3fffdc74e29c93e42db4fc8520c5
Jeronimo <jeronimotornini@gmail.com>
2023-05-28 22:03:27 -0300

    Arreglo de Posicion de Footer y Cambios de Card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,12 +10,13 @@ function Card({ Data }) {
   return (
     <Link
       href={`./details`}
-      className="flex flex-col bg-slate-50 items-center backdrop-blur-sm w-40 h-56 justify-center rounded-lg p-2 gap-y-3 shadow-sm shadow-zinc-400 hover:scale-110 hover:shadow-lg hover:shadow-emerald-400"
+      className="flex flex-col bg-slate-50 items-center backdrop-blur-sm w-44 h-64 justify-center rounded-lg p-2 gap-y-3 shadow-sm shadow-zinc-400 hover:scale-110 hover:shadow-lg hover:shadow-emerald-400"
       key={Data.id}
       onClick={() => Dispatch(ProductSlice.actions.SetDetails(Data.id))}
     >
       <Image src={Data.image} width={100} height={100} alt="Image" />
-      <div className="flex items-center flex-col mt-auto">
+      <div className="flex items-center flex-col mt-auto gap-y-1">
+        <p className="text-zinc-500 text-md">{Data.category}</p>
         <p className="text-black font-bold text-lg">{Data.name}</p>
         <p className="text-black">$ {Data.price}</p>
       </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return <div className="flex flex-col gap-y-2 items-center mt-7 mb-2">
+  return <div className="flex flex-col gap-y-2 items-center mt-auto mb-2">
     <p className="text-lg text-zinc-600">E-Commerce</p>
     <p className="text-zinc-600">Copyright© 2023</p>
   </div>;
