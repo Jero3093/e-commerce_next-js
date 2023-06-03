fbaa63a2ad7e3f0eec37f686139cb21c1c1d765b
Jeronimo <jeronimotornini@gmail.com>
2023-06-03 00:11:55 -0300

    Arreglos en Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import Icon from "../Icons/Icon.png";
-import { IoPersonCircleOutline, IoSettingsSharp, IoMenuOutline } from "react-icons/io5";
-import { useDispatch } from "react-redux";
+import {
+  IoPersonCircleOutline,
+  IoSettingsSharp,
+  IoMenuOutline,
+} from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
 import { MenuSlice } from "@/app/Redux/MenuSlice";
+import { UserSlice } from "@/app/Redux/UserSlice";
+import { CartItemsCount } from "@/app/Redux/CartSlice";
 import Link from "next/link";
 
-function Header({ User }) {
+function Header() {
+  useEffect(() => {
+    GetUserSession();
+  }, []);
+
+  const GetUserSession = () => {
+    const userSession = JSON.parse(localStorage.getItem("userSession"));
+    if (userSession) {
+      Dispatch(
+        UserSlice.actions.SetUser({
+          userId: userSession.userId,
+          userEmail: userSession.userEmail,
+        })
+      );
+    }
+  }; //Get the Current User Session from the Local Storage
+
+  const CurrentUser = useSelector((state) => state.UserSlice.User); //Current User State
+
+  const CartCount = useSelector(CartItemsCount);
+
   const Dispatch = useDispatch(); //Dispatch Hook
 
   return (
@@ -20,7 +46,7 @@ function Header({ User }) {
       </div>
       {/* Logo */}
       <div className="flex flex-row items-center gap-x-3">
-        <Image src={Icon} className="w-14 h-14 drop-shadow-md" />
+        <Image src={Icon} className="w-14 h-14 drop-shadow-md" alt="Logo" />
         <p className="font-bold text-2xl hidden md:flex text-white">
           E-Commerce
         </p>
@@ -28,31 +54,39 @@ function Header({ User }) {
       {/* Menu Links */}
       <div className="flex-row items-center gap-x-4 mt-4 hidden lg:flex">
         <Link
-          href="./"
+          href={"/"}
           className="text-xl font-bold hover:text-2xl hover:border-b-2 border-white text-white"
-          onClick={() => Dispatch(MenuSlice.actions.SetMenu(false))}
         >
           Home
         </Link>
         <Link
-          href={"./contact"}
+          href={"/contact"}
           className="text-xl font-bold hover:text-2xl hover:border-b-2 border-white text-white "
-          onClick={() => Dispatch(MenuSlice.actions.SetMenu(false))}
         >
           Contact
         </Link>
-        <Link
-          href={"#"}
-          className="text-xl font-bold hover:text-2xl hover:border-b-2 border-white text-white"
-          onClick={() => Dispatch(MenuSlice.actions.SetMenu(false))}
-        >
-          My Cart
-        </Link>
+        {CurrentUser ? (
+          <Link
+            href={"/cart"}
+            className="text-xl font-bold hover:text-2xl hover:border-b-2 border-white text-white relative"
+          >
+            My Cart
+            {CartCount > 0 ? (
+              <p className="text-rose-500 rounded-full absolute bottom-5 -right-3">
+                {CartCount}
+              </p>
+            ) : (
+              ""
+            )}
+          </Link>
+        ) : (
+          ""
+        )}
       </div>
       {/* User Button */}
-      {User ? (
+      {CurrentUser ? (
         <Link href={"/user"}>
-          <IoSettingsSharp className="w-10 h-10 text-zinc-300"  />
+          <IoSettingsSharp className="w-10 h-10 text-zinc-300" />
           {/* <Image src={UserConfigIcon} alt="user" width={47} height={47} /> */}
         </Link>
       ) : (
