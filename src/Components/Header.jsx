64b20bf21c57109449844d6b0c645f793c094f79
Jeronimo <jeronimotornini@gmail.com>
2023-06-08 22:49:10 -0300

    Header con Animaciones

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { MenuSlice } from "@/app/Redux/MenuSlice";
 import { UserSlice } from "@/app/Redux/UserSlice";
 import { CartItemsCount } from "@/app/Redux/CartSlice";
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 function Header() {
@@ -38,62 +39,103 @@ function Header() {
   return (
     <div className="flex flex-row justify-between p-2 px-3 items-center">
       {/* Menu Button */}
-      <div
+      <motion.div
+        initial={{ x: -3, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 0.3 }}
         onClick={() => Dispatch(MenuSlice.actions.SetMenu(true))}
         className="cursor-pointer hover:animate-pulse lg:hidden"
       >
         <IoMenuOutline className="w-14 h-14 text-zinc-300" />
-      </div>
+      </motion.div>
       {/* Logo */}
-      <div className="flex flex-row items-center gap-x-3">
+      <motion.div
+        className="flex flex-row items-center gap-x-3"
+        initial={{ y: -3, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.2 }}
+      >
         <Image src={Icon} className="w-14 h-14 drop-shadow-md" alt="Logo" />
         <p className="font-bold text-2xl hidden md:flex text-white">
           E-Commerce
         </p>
-      </div>
+      </motion.div>
       {/* Menu Links */}
       <div className="flex-row items-center gap-x-4 mt-4 hidden lg:flex">
-        <Link
-          href={"/"}
-          className="text-xl font-bold hover:text-2xl hover:border-b-2 border-white text-white"
+        {/* Home */}
+        <motion.div
+          initial={{ y: -5, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.3 }}
         >
-          Home
-        </Link>
-        <Link
-          href={"/contact"}
-          className="text-xl font-bold hover:text-2xl hover:border-b-2 border-white text-white "
-        >
-          Contact
-        </Link>
-        {CurrentUser ? (
           <Link
-            href={"/cart"}
-            className="text-xl font-bold hover:text-2xl hover:border-b-2 border-white text-white relative"
+            href={"/"}
+            className="text-xl font-bold hover:text-2xl hover:border-b-2 border-white text-white"
           >
-            My Cart
-            {CartCount > 0 ? (
-              <p className="text-rose-500 rounded-full absolute bottom-5 -right-3">
-                {CartCount}
-              </p>
-            ) : (
-              ""
-            )}
+            Home
           </Link>
+        </motion.div>
+        {/* Contact */}
+        <motion.div
+          initial={{ y: -5, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.4 }}
+        >
+          <Link
+            href={"/contact"}
+            className="text-xl font-bold hover:text-2xl hover:border-b-2 border-white text-white "
+          >
+            Contact
+          </Link>
+        </motion.div>
+        {/* Cart */}
+        {CurrentUser ? (
+          <motion.div
+            initial={{ y: -5, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.5 }}
+          >
+            <Link
+              href={"/cart"}
+              className="text-xl font-bold hover:text-2xl hover:border-b-2 border-white text-white relative"
+            >
+              My Cart
+              {CartCount > 0 ? (
+                <p className="text-rose-500 rounded-full absolute bottom-5 -right-3">
+                  {CartCount}
+                </p>
+              ) : (
+                ""
+              )}
+            </Link>
+          </motion.div>
         ) : (
           ""
         )}
       </div>
       {/* User Button */}
       {CurrentUser ? (
-        <Link href={"/user"}>
-          <IoSettingsSharp className="w-10 h-10 text-zinc-300" />
-          {/* <Image src={UserConfigIcon} alt="user" width={47} height={47} /> */}
-        </Link>
+        <motion.div
+          initial={{ x: 3, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.3 }}
+        >
+          <Link href={"/user"}>
+            <IoSettingsSharp className="w-10 h-10 text-zinc-300" />
+            {/* <Image src={UserConfigIcon} alt="user" width={47} height={47} /> */}
+          </Link>
+        </motion.div>
       ) : (
-        <Link href={"/login"}>
-          <IoPersonCircleOutline className="w-12 h-12 text-zinc-300" />
-          {/* <Image src={UserIcon} width={47} height={47} alt="user" /> */}
-        </Link>
+        <motion.div
+          initial={{ x: 3, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.3 }}
+        >
+          <Link href={"/login"}>
+            <IoPersonCircleOutline className="w-12 h-12 text-zinc-300" />
+            {/* <Image src={UserIcon} width={47} height={47} alt="user" /> */}
+          </Link>
+        </motion.div>
       )}
     </div>
   );
