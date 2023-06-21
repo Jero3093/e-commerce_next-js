144a124464be6ba69ca3574743408e4d0b462bfc
Jeronimo <jeronimotornini@gmail.com>
2023-06-21 17:25:26 -0300

    Cambio en UserSlice para incluir el cambo admin

--- src/app/Redux/UserSlice.js
+++ src/app/Redux/UserSlice.js
@@ -10,9 +10,9 @@ export const UserSlice = createSlice({
   initialState,
   reducers: {
     SetUser: (state, action) => {
-      const { userId, userEmail } = action.payload;
+      const { userId, userEmail, admin } = action.payload;
 
-      state.User = { userId, userEmail };
+      state.User = { userId, userEmail, admin };
     },
     LogOutUser: (state) => {
       state.User = null;
