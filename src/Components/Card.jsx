2a1dd9af0f1a7ecf0691fb536e6dd84312d174c9
Jeronimo <jeronimotornini@gmail.com>
2023-06-22 21:55:51 -0300

    Cambios de Diseño en Card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,15 +10,15 @@ function Card({ Data }) {
   return (
     <Link
       href={`./details`}
-      className="flex flex-col bg-transparent items-center backdrop-blur-sm w-52 h-96 justify-center rounded-lg p-2 gap-y-3 hover:scale-110 hover:shadow-md hover:shadow-emerald-400"
+      className="flex flex-col bg-white items-center backdrop-blur-sm w-52 h-96 justify-center rounded-lg p-2 gap-y-3 hover:scale-110 hover:shadow-lg hover:shadow-emerald-400"
       key={Data.id}
       onClick={() => Dispatch(ProductSlice.actions.SetDetails(Data.id))}
     >
-      <Image src={Data.image} width={174} height={100} alt="Image" />
+      <Image src={Data.image} width={174} height={174} alt="Image" />
       <div className="flex items-center flex-col mt-auto gap-y-1">
         <p className="text-zinc-400 text-lg">{Data.category}</p>
-        <p className="text-white font-bold text-xl">{Data.name}</p>
-        <p className="text-white text-lg">$ {Data.price}</p>
+        <p className="text-black font-bold text-xl">{Data.name}</p>
+        <p className="text-black text-lg">$ {Data.price}</p>
       </div>
     </Link>
   );
