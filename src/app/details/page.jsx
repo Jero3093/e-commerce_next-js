017414786f6772d995645915490addad5de51b70
Jeronimo <jeronimotornini@gmail.com>
2023-07-07 19:09:22 -0300

    Funciones de traer productos de base de datos

--- src/app/details/page.jsx
+++ src/app/details/page.jsx
@@ -1,14 +1,23 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useSelector, useDispatch } from "react-redux"; //Redux
 import { Toaster, toast } from "sonner"; //Notification
 import { CartSlice } from "../Redux/CartSlice"; //Cart Slice
+import { motion } from "framer-motion";
 import Header from "@/Components/Header"; //Header Component
 import Menu from "@/Components/Menu"; //Menu Component
 import Footer from "@/Components/Footer"; //Footer Component
 
 export default function Details() {
+  useEffect(() => {
+    if (!CurrentUser || !Product) {
+      Router.replace("/");
+      return;
+    }
+  }, []);
+
   const CurrentUser = useSelector((state) => state.UserSlice.User); //Current User Session State
 
   const Product = useSelector((state) => state.ProductSlice.Details); //Product Slice
@@ -17,8 +26,10 @@ export default function Details() {
 
   const Dispatch = useDispatch(); //Dispatch Hook
 
+  const Router = useRouter(); //Router Hook
+
   const AddToCart = () => {
-    const promise = () => new Promise((resolve) => setTimeout(resolve, 2000));
+    const promise = () => new Promise((resolve) => setTimeout(resolve, 1000));
     if (!CurrentUser) {
       toast.error("You must be logged in to add a product to your cart");
     } else {
@@ -41,15 +52,26 @@ export default function Details() {
         <>
           <Toaster position="top-center" richColors />
           {/* Header */}
-          <Header User={CurrentUser} />
+          <Header />
           {/* Product Content */}
           <main className="flex flex-col mt-8 md:flex-row md:mx-6 md:gap-x-4 md:mb-5 md:self-center">
             {/* Image */}
-            <div className="bg-white w-72 h-full shadow-lg shadow-zinc-500 p-5 border-2 border-zinc-400 rounded-lg self-center">
-              <Image src={Product.image} width={250} height={260} alt="photo" />
-            </div>
+            <motion.div
+              className="flex bg-white w-full h-full shadow-lg shadow-zinc-500 p-5 border-2 border-zinc-400 rounded-lg self-center items-center
+              md:max-w-full md:h-96 justify-center lg:max-w-fit"
+              initial={{ x: -100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ delay: 0.1 }}
+            >
+              <Image src={Product.image} width={320} height={320} alt="photo"  />
+            </motion.div>
             {/* Information */}
-            <section className="flex flex-col gap-y-3 p-4 mt-4 lg:gap-y-5">
+            <motion.section
+              className="flex flex-col gap-y-3 p-4 mt-4 lg:gap-y-5"
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ delay: 0.2 }}
+            >
               <p className="text-lg text-zinc-400">{Product.category}</p>
               <p className="text-2xl text-white">{Product.name}</p>
               <p className="text-xl text-white">{Product.description}</p>
@@ -57,15 +79,18 @@ export default function Details() {
               <p className="text-xl font-bold text-white">
                 Stock: {Product.stock}
               </p>
-            </section>
+            </motion.section>
           </main>
           {/* Cart Button */}
-          <button
+          <motion.button
             className="flex items-center justify-center self-center w-80 h-12 rounded-full bg-emerald-500 shadow-lg shadow-emerald-800 mt-2 mb-5"
             onClick={AddToCart}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.3 }}
           >
             <p className="text-2xl font-bold">Add to Cart</p>
-          </button>
+          </motion.button>
           {/* Footer */}
           <Footer />
         </>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,45 +1,65 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { UserSlice } from "./Redux/UserSlice";
-import { Products } from "@/Data/Products";
-import Header from "@/Components/Header";
-import Menu from "@/Components/Menu";
-import Footer from "@/Components/Footer";
-import Card from "@/Components/Card";
+import { useSelector, useDispatch } from "react-redux"; //Redux
+import { Audio } from "react-loader-spinner"; //Loader Spinner
+import { toast, Toaster } from "sonner"; //Notifications
+import { ProductSlice } from "./Redux/ProductSlice"; //Product Slice
+import { database } from "@/firebase/Firebase"; //Firebase Database
+import { collection, getDocs, query, where } from "firebase/firestore"; //Firebase Firestore
+import { motion } from "framer-motion"; //Framer Motion
+import Header from "@/Components/Header"; //Header
+import Menu from "@/Components/Menu"; //Menu
+import Footer from "@/Components/Footer"; //Footer
+import Card from "@/Components/Card"; //Card
+import Banner from "@/Components/Banner"; //Banner
 
 export default function Home() {
   useEffect(() => {
-    GetUserSession();
+    GetProductsFromDB();
   }, []);
+
+  const [IsLoading, setIsLoading] = useState(false); //Loading State
 
   const [SearchText, setSearchText] = useState(""); //Input Text Value State
 
-  const GetUserSession = () => {
-    const userSession = JSON.parse(localStorage.getItem("userSession"));
-    if (userSession) {
-      Dispatch(
-        UserSlice.actions.SetUser({
-          userId: userSession.userId,
-          userEmail: userSession.userEmail,
-        })
-      );
-    }
-  }; //Get the Current User Session from the Local Storage
-
-  const Dispatch = useDispatch(); //Dispatch Hook
-
-  const CurrentUser = useSelector((state) => state.UserSlice.User); //Current User State
+  const DBProducts = useSelector((state) => state.ProductSlice.DBProducts); //Products State
 
   const MenuOpen = useSelector((state) => state.MenuSlice.Open); //Menu State
 
-  const FilteredProducts = Products.filter((product) =>
+  const Dispatch = useDispatch(); //Dispatch
+
+  const GetProductsFromDB = async () => {
+    try {
+      Dispatch(ProductSlice.actions.SetCleaerDBProducts());
+      setIsLoading(true);
+      await getDocs(
+        query(collection(database, "products"), where("available", "==", true))
+      ).then((products) => {
+        if (products.empty) {
+          toast.error("No products available");
+          console.log("No products available");
+          setIsLoading(false);
+          return;
+        }
+        products.forEach((product) => {
+          let data = { ...product.data(), id: product.id };
+          Dispatch(ProductSlice.actions.SetDBProducts(data));
+        });
+      });
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error.message);
+    }
+  }; //Gets the products from the database
+
+  const FilteredProducts = DBProducts.filter((product) =>
     product.name.toLowerCase().includes(SearchText.toLowerCase())
   ); //Filters the array of products using the search text
 
   const ProductsList = () => {
     if (SearchText === "") {
-      return Products.map((product) => {
+      return DBProducts.map((product) => {
         return <Card Data={product} />;
       });
     } else {
@@ -47,7 +67,7 @@ export default function Home() {
         return <Card Data={product} />;
       });
     }
-  };
+  }; //Returns the products list
 
   return (
     <main className="flex min-h-screen flex-col lg:pl-36 lg:pr-36 bg-sky-950">
@@ -55,19 +75,58 @@ export default function Home() {
         <Menu />
       ) : (
         <>
-          <Header User={CurrentUser} />
-          {/* Input Search */}
-          <input
-            type="text"
-            className="w-5/6 h-12 p-3 self-center mt-3 bg-transparent border-b-2 border-zinc-400 placeholder:text-zinc-400 outline-none hover:border-2 hover:rounded-lg text-white"
-            placeholder="Search for products..."
-            value={SearchText}
-            onChange={(text) => setSearchText(text.target.value)}
-          />
-          {/* Products List */}
-          <div className="grid flex-col grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 place-items-center mt-7 gap-y-7 pb-4">
-            <ProductsList />
-          </div>
+          {/* Notifications */}
+          <Toaster richColors position="top-center" />
+          {/* Header */}
+          <Header />
+          {IsLoading ? (
+            <div className="flex items-center justify-center mt-96">
+              <Audio
+                height="70"
+                width="70"
+                radius="9"
+                color="white"
+                ariaLabel="loading"
+                wrapperStyle
+                wrapperClass
+              />
+            </div>
+          ) : (
+            <>
+              {/* Banner */}
+              <Banner />
+              {/* Input Search */}
+              <motion.input
+                type="text"
+                className="w-5/6 h-12 p-3 self-center mt-3 bg-transparent border-b-2 border-zinc-400 placeholder:text-zinc-400 outline-none hover:border-2 hover:rounded-lg text-white"
+                placeholder="Search products..."
+                value={SearchText}
+                onChange={(text) => setSearchText(text.target.value)}
+                initial={{ x: 100, opacity: 0 }}
+                animate={{ opacity: 1, x: 0 }}
+              />
+              {DBProducts.length === 0 ? (
+                <motion.h1
+                  className="text-2xl text-white font-semibold self-center mt-6"
+                  initial={{ y: -10, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: 0.3 }}
+                >
+                  No Products Available
+                </motion.h1>
+              ) : (
+                // Products List
+                <motion.div
+                  className="grid flex-col grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 place-items-center mt-9 gap-y-1 md:gap-y-9 pb-4"
+                  initial={{ y: -5, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: 0.2 }}
+                >
+                  <ProductsList />
+                </motion.div>
+              )}
+            </>
+          )}
           {/* Footer */}
           <Footer />
         </>
